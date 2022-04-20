eb68b43102c9b3b1cb956f9507529f1834369fec
Kamil <krzysiu.jedrzejewski@gmail.com>
2022-04-20 16:58:53 +0200

    Zmiany formularzowe

--- formularz.spec.js
+++ formularz.spec.js
@@ -2,18 +2,21 @@
 
 describe('RadioButtonAndCheckbox', () =>{
     it('First try', ()=>{
-        // cy.visit('https://formularze.uniqa.pl/UniqaForms/formularz-kontaktowy')
+
         cy.visit('https://www.stypendiadladziewczyn.pl/pl/studentka')
 
         cy.wait(2000)
 
 
 
-        cy.get('[type="radio"]').last().click({force:true})
+        cy.get('[type="radio"]')
+            .last()
+            .click({force:true})
 
         cy.get('[type="checkbox"]')
-            .first()
+            .eq(1)
             .click({force:true})
+        
         
 
         
