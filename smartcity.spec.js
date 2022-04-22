5b8479e28ecd855bc0e507879b9684210688e588
Kamil <krzysiu.jedrzejewski@gmail.com>
2022-04-22 15:17:40 +0200

    Smart

---
+++ smartcity.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('SmartCityForum',()=>{
+
+
+    it('Krok 1', ()=>{ 
+        let website = "https://forumbranzowe.com/"
+
+        /// wejście na strone
+        cy.visit(website)
+
+        cy.contains("Oferty pracy")
+            .click()
+
+        cy.get('[id="dolnoslaskie"]')
+            .click({force:true})
+
+    })
+})
