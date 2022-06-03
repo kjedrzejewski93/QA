701f2c5fa47271096ce90e1bdbc15e99398f1c3a
Kamil <krzysiu.jedrzejewski@gmail.com>
2022-06-03 11:52:05 +0200

    WIP

---
+++ future.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('BnkToTheFuture',()=>{
+
+
+    it('Website', ()=>{ 
+        let website = "https://bnktothefuture.com/"
+
+        /// wejście na strone
+        cy.visit(website)
+
+
+    })
+})
